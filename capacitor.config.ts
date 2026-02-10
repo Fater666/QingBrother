a2e29d1a0af3d44c1c0b9af643878244c65a3a88
Fater66 <664327117@qq.com>
2026-02-11 00:34:56 +0800

    feat:安卓打包

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.qingbrother.app',
+  appName: 'QingBrother',
+  webDir: 'dist'
+};
+
+export default config;
